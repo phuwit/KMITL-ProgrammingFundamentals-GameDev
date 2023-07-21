4db9a4b9b89abe4fd1c6054a14933ac10813160a
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-07-21 22:14:45 +0700

    book-pong : make ball bigger

--- figuringitout/book/pong/Ball.cpp
+++ figuringitout/book/pong/Ball.cpp
@@ -5,7 +5,7 @@ Ball::Ball(float startX, float startY) {
     m_Position.x = startX;
     m_Position.y = startY;
 
-    m_Shape.setSize(sf::Vector2f(10, 10));
+    m_Shape.setSize(sf::Vector2f(20, 20));
     m_Shape.setPosition(m_Position);
 }
 

e6db08f60d3c69ff1ee3b811e5f40accc380876b
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2023-08-28 17:22:53 +0700

    autoformat and making sense of the code

--- figuringitout/colorgradient/main.cpp
+++ figuringitout/colorgradient/main.cpp
@@ -1,38 +1,38 @@
 #include <SFML/Graphics.hpp>
+
 #include "ColorScale.cpp"
 
-int main()
-{
-	
-	const int SIZE = 500;
-	ColorScale gradient;
-	gradient.insert(0.0,sf::Color::Red);
-	gradient.insert(1.0,sf::Color::Magenta);
-	gradient.insert(2.0,sf::Color::Blue);
-	gradient.insert(3.0,sf::Color::Cyan);
-	gradient.insert(4.0,sf::Color::Green);
-	gradient.insert(5.0,sf::Color::Yellow);
-	gradient.insert(6.0,sf::Color::Red);
-	
-	
-	sf::Image image;
+int main() {
+    const int SIZE = 500;
+    ColorScale gradient;
+    gradient.insert(0.0, sf::Color::Red);
+    gradient.insert(1.0, sf::Color::Magenta);
+    gradient.insert(2.0, sf::Color::Blue);
+    gradient.insert(3.0, sf::Color::Cyan);
+    gradient.insert(4.0, sf::Color::Green);
+    gradient.insert(5.0, sf::Color::Yellow);
+    gradient.insert(6.0, sf::Color::Red);
+
+    sf::Image image;
     image.create(SIZE, SIZE, sf::Color::Black);
-	gradient.draw(image,sf::Vector2f(250.0,250.0),sf::Vector2f(200.0,0.0),GradientStyle::Circle);
+    // draw circular gradient into an image : params = image reference, circle center, width of each band, gradient style
+    gradient.draw(image, sf::Vector2f(500.0, 500.0), sf::Vector2f(100.0, 0.0),
+                  GradientStyle::Circle);
     sf::Texture imageTexture;
     imageTexture.loadFromImage(image);
-	
-	sf::Sprite sprite(imageTexture);
-	sf::RenderWindow App(sf::VideoMode(SIZE, SIZE, 32), "Gradient");
-	App.draw(sprite);
-	App.display();
-	App.setFramerateLimit(1);
-	
-	while(App.isOpen())
-	{
-		sf::Event event;
-		while(App.pollEvent(event));
-		App.display();
-	}
-	
-	return 0;
+
+    sf::Sprite sprite(imageTexture);
+    sf::RenderWindow App(sf::VideoMode(SIZE, SIZE, 32), "Gradient");
+    App.draw(sprite);
+    App.display();
+    App.setFramerateLimit(1);
+
+    while (App.isOpen()) {
+        sf::Event event;
+        while (App.pollEvent(event))
+            ;
+        App.display();
+    }
+
+    return 0;
 }
